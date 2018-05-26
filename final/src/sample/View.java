c8974ba7284f3df0beb9c1b402083396d5f9354e
Chiraag Gohel <cgohel@gmail.com>
2018-05-25 19:10:38 -0500

    Yes

--- final/src/sample/View.java
+++ final/src/sample/View.java
@@ -1,4 +1,17 @@
 package sample;
 
-public class View {
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class View extends Group {
+
+    public View() {
+
+    }
+
+    public void update(Model model) {
+
+    }
 }
