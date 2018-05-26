23855b6847e63ff4bc12f4b0b9c4f0bc5d64dfa6
Sharan Ganjam Seshachallam <sharanseshachallam@eduroam-168-28.dyn.carleton.edu>
2018-05-25 19:43:52 -0500

    view updated

--- final/src/sample/View.java
+++ final/src/sample/View.java
@@ -7,11 +7,46 @@ import javafx.scene.image.ImageView;
 
 public class View extends Group {
 
-    public View() {
+    /* A get method for the size of the pendulum system.
+    */
+    public int getN() {
+        return this.N;
+    }
+
+    /* A set method for the size of the pendulum system.
+    */
+    public void setN(int N) {
+        this.N = N;
+        this.initializeView();
+    }
+
+    /* Initializes the graphic of the pendulum system.
+      * Graphic starts from the horizontal position.
+      * Includes a timer to show the evolution of the system quantitatively.
+    */
+    public initializeView() {
 
     }
 
-    public void update(Model model) {
+    /* Initializes the data graph of the Kinetic energy of the pendulum system.
+      * Graphic starts from the horizontal position.
+      * Includes a timer to show the evolution of the system quantitatively.
+    */
+    public initializeGraph() {
+
+    }
+
+    /* Updates the graph view with information from model.
+    * Will show different models as user inputs different demands.
+    */
+    public void updateGraph(Model model) {
+
+    }
+
+    /* Updates the pendulum visual with information from model.
+    * Will show different system as user inputs different demands.
+    */
+    public void updateView(Model model) {
 
     }
 }
