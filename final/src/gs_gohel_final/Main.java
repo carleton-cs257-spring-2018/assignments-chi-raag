5f5e1def3ba3e1f2462c77bf0b3827f381ddd5f5
Chiraag Gohel <cgohel@gmail.com>
2018-05-30 15:06:01 -0500

    fixed Main FXML error

--- final/src/gs_gohel_final/Main.java
+++ final/src/gs_gohel_final/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("npendulum.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gs_gohel_final.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("The Pendulum");
 
