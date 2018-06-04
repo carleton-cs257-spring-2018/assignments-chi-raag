131cdad2b92d9b557417b1d2626f3963b9b08e96
Sharan Ganjam Seshachallam <sharanseshachallam@eduroam-168-28.dyn.carleton.edu>
2018-06-04 11:14:15 -0500

    mujhe maaf kardo

--- final/src/gs_gohel_final/View.java
+++ final/src/gs_gohel_final/View.java
@@ -24,6 +24,7 @@ public class View extends Pane {
     private double theta;
     @FXML Circle circle1;
     Circle circle2;
+    Circle circle3;
     Line line;
     Line line2;
     double x;
@@ -42,20 +43,21 @@ public class View extends Pane {
      */
     public View() {
         Model model = new Model();
-
+        System.out.println(model.nodes);
 
         circle1 = new Circle(this.getMaxWidth()/2, this.getMaxHeight()/2, 14);
 
 
         // Create another circle
         circle2 = new Circle(300, 200+model.getyCoordinate(), 5);
+        //circle3 = new Circle(this.getMaxWidth()/1.5, this.getMaxHeight()/1.5, 14);
+        circle3 = new Circle(circle1.getCenterX(), circle1.getCenterY(),14);
 
         line = new Line(circle1.getCenterX(), circle1.getCenterY(), 300, circle2.getCenterY());
         line.setStrokeWidth(3);
         // Binding the line and the circle1 together, so they move synchronized
         line.startXProperty().bind(circle1.centerXProperty().add(circle1.translateXProperty()));
         line.startYProperty().bind(circle1.centerYProperty().add(circle1.translateYProperty()));
-        circle3 = new Circle(this.getMaxWidth()/1.5, this.getMaxHeight()/1.5, 14);
         line2 = new Line(circle3.getCenterX(), circle3.getCenterY(), circle1.getCenterX(), circle1.getCenterY());
         line2.startXProperty().bind(circle3.centerXProperty().add(circle3.translateXProperty()));
         line2.startYProperty().bind(circle3.centerYProperty().add(circle3.translateYProperty()));
@@ -78,11 +80,11 @@ public class View extends Pane {
         @param model: the pendulum object
      */
     public void update(Model model) {
-        System.out.println(model.getxCoordinate2());
+        //System.out.println(model.getxCoordinate2());
         x = model.getxCoordinate() * this.getScene().getWidth() * .03 + this.getScene().getWidth()*0.5;
         y = 200+ model.getyCoordinate() * this.getScene().getHeight() * .03;
         x2 = model.getxCoordinate2() * this.getScene().getWidth() * .03 + this.getScene().getWidth()*0.5;
-        y2 = 300+ model.getyCoordinate2() * this.getScene().getHeight() * .03;
+        y2 = 200+ model.getyCoordinate2() * this.getScene().getHeight() * .03;
         //System.out.println(x2);
         //System.out.println(y2);
 
